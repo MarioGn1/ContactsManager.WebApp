7131c34f379bab8d121a9b30d4da5ced89c5c209
Mario <mariogn@mail.bg>
2022-03-28 11:34:21 +0300

    Login/Register css

--- src/ContactsManager.ReactClient/contacts-manager-client/src/components/Login.js
+++ src/ContactsManager.ReactClient/contacts-manager-client/src/components/Login.js
@@ -13,27 +13,31 @@ const Login = ({
 
         history.push('/')
         // historyHook.push('/games');
-    };      
-    
+    };
+
     return (
-        <section id="login-page" className="auth">
-            <form id="login" onSubmit={onFormSubmit}>
+        <>
+            <section id="login-page" className="auth">
+                <form id="login" onSubmit={onFormSubmit}>
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+                    <div className="container">
+                        <div className="brand-logo"></div>
+                        <h1>Login</h1>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
-                    <input type="submit" className="btn submit" value="Login" />
-                    <p className="field">
-                        <span>If you don't have profile click <NavLink activeClassName="active-navigation-link" to="/register">here</NavLink></span>
-                    </p>
-                </div>
-            </form>
-        </section>
+                        <label htmlFor="login-pass">Password:</label>
+                        <input type="password" id="login-password" name="password" />
+                        <input type="submit" className="btn submit" value="Login" />
+
+                    </div>
+                </form>
+            </section>
+            <p className="field">
+                <span>If you don't have profile click <NavLink activeClassName="active-navigation-link" to="/register">here</NavLink></span>
+            </p>
+        </>
+
     );
 }
 
